c0c87a278d22c6a2d25ca0467f411bd7efe13cef
Brandon Seaver <seaverb@icloud.com>
2026-03-19 14:12:36 -0400

    Add --json flag to version command

--- cmd/version.go
+++ cmd/version.go
@@ -1,21 +1,37 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+var versionJSON bool
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print wzrd-vault version information",
 	Long:  "Print the version, git commit, and build date of the wzrd-vault binary.",
 	Args:  cobra.NoArgs,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if versionJSON {
+			out := map[string]string{
+				"version": versionStr,
+				"commit":  commitStr,
+				"date":    dateStr,
+			}
+			enc := json.NewEncoder(os.Stdout)
+			enc.SetIndent("", "  ")
+			return enc.Encode(out)
+		}
 		fmt.Printf("wzrd-vault version %s (commit %s, built %s)\n", versionStr, commitStr, dateStr)
+		return nil
 	},
 }
 
 func init() {
+	versionCmd.Flags().BoolVar(&versionJSON, "json", false, "output as JSON")
 	rootCmd.AddCommand(versionCmd)
 }
